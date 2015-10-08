52a4e5e0e857cf76f5e698dc31d77a5a3a9df9e4
David Gudeman <davidmgudeman@gmail.com>
2015-10-08 15:21:26 -0700

    first commit

---
+++ CIS55Lab1C/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CIS55Lab1C
+//
+//  Created by David Gudeman on 10/8/15.
+//  Copyright (c) 2015 DeAnza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet var centsLabel: UILabel!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
