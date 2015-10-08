9735e1b7cb6bb19a2368d17c45b7a5aa252a87b9
David Gudeman <davidmgudeman@gmail.com>
2015-10-08 15:37:47 -0700

    compiles

--- CIS55Lab1C/ViewController.swift
+++ CIS55Lab1C/ViewController.swift
@@ -9,18 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet var daysLabel: UILabel!
+  @IBOutlet var daysText: UITextField!
   @IBOutlet var centsLabel: UILabel!
+  @IBOutlet var centsText: UITextField!
+  @IBOutlet var outputLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  @IBAction func calcButton(sender: AnyObject) {
+  }
+  
 }
 
